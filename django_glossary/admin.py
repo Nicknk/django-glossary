3649ebe19690de4a759842870f5f0a882f20bd9a
Nick N Krasnikov <n47@paragenesis.ru>
2020-11-20 22:31:23 +0300

    Use inline synonyms for terms

--- django_glossary/admin.py
+++ django_glossary/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django_glossary.models import Term, Synonym
 
+class SynonymsInline(admin.StackedInline):
+    model = Synonym
+    extra = 1
 
 @admin.register(Term)
 class TermAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['title']}
     list_display = ('title',)
     search_fields = ('title', 'description')
+    inlines = [SynonymsInline]
 
 
 @admin.register(Synonym)
